6d2c822656f5206d49f1d4866392fcc3e78fbd07
AzumattDev <azumattgaming@gmail.com>
2022-05-17 00:05:24 -0400

    Fix issue created in 1.5.0

--- Patches/Player.cs
+++ Patches/Player.cs
@@ -9,14 +9,27 @@ namespace XRayVision.Patches
     {
         static void Postfix(Player __instance)
         {
-            if (ZNet.instance.IsServer() || ZNet.instance.IsDedicated()) return;
-            string? steamID = readLocalSteamID();
-            if (__instance.m_nview.GetZDO() == null)
-                return;
-            __instance.m_nview?.GetZDO()
-                .Set("steamName", SteamFriends.GetPersonaName());
-            __instance.m_nview?.GetZDO()
-                .Set("steamID", steamID);
+            if (!ZNet.instance) return;
+            if (ZNet.instance.IsServer() && ZNet.instance.IsDedicated())
+            {
+                ZNetPeer? netPeer = ZNet.instance.GetPeer(__instance.m_nview.m_zdo.m_uid.m_userID);
+
+                CSteamID SteamID = new(ulong.Parse(netPeer.m_rpc.GetSocket().GetHostName()));
+                __instance.m_nview?.GetZDO()
+                    .Set("steamName", SteamFriends.GetFriendPersonaName(SteamID));
+                __instance.m_nview?.GetZDO()
+                    .Set("steamID", netPeer.m_rpc.GetSocket().GetHostName());
+            }
+            else
+            {
+                string? steamID = readLocalSteamID();
+                if (__instance.m_nview.GetZDO() == null)
+                    return;
+                __instance.m_nview?.GetZDO()
+                    .Set("steamName", SteamFriends.GetPersonaName());
+                __instance.m_nview?.GetZDO()
+                    .Set("steamID", steamID);
+            }
         }
 
         internal static string? readLocalSteamID() =>
